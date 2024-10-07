bf17b5ab4f0b54ca12d88326223c6d55f049e63d
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-07 13:45:10 -0500

    created function bothStringsIncluded

--- problems/04-both-strings-included.js
+++ problems/04-both-strings-included.js
@@ -6,11 +6,21 @@ it should return false.
 */
 
 // Your code here 
+// Define a function named bothStringsIncluded that accepts a sentence string,
+// and two strings as parameters (for a total of 3 parameters)
 
-// console.log(bothStringsIncluded("how now brown cow?", "panther", "cow"));  //=> false
-// console.log(bothStringsIncluded("Dance party!", "Dance", "party"));        //=> true
-// console.log(bothStringsIncluded("Question?", "tion", "?"));                //=> true
-// console.log(bothStringsIncluded("I love programming", "apple", "potato")); //=> false
+function bothStringsIncluded(sentence, str1, str2) {
+    // if both strings are found in the sentence, return true
+    if (sentence.includes(str1) && sentence.includes(str2)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(bothStringsIncluded("how now brown cow?", "panther", "cow"));  //=> false
+console.log(bothStringsIncluded("Dance party!", "Dance", "party"));        //=> true
+console.log(bothStringsIncluded("Question?", "tion", "?"));                //=> true
+console.log(bothStringsIncluded("I love programming", "apple", "potato")); //=> false
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = bothStringsIncluded;
