cd6536eb9ec797b7697512b6f92367723ebeb6a5
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-07 13:35:33 -0500

    added isFive function

--- problems/01-is-five.js
+++ problems/01-is-five.js
@@ -4,10 +4,17 @@ a string of 'is five' if the number is equal to 5 and `null` if it is not.
 */
 
 // Your code here 
+// create a function that takes in a number parameter
+function isFive(num1) {
+    // if the number is equal to 5, return 'is five'
+    if (num1 === 5) {
+        return 'is five';
+    }
+}
 
-// console.log(isFive(5));      // => 'is five'
-// console.log(isFive(13));     // => null
-// console.log(isFive("apple")) // => null
+console.log(isFive(5));      // => 'is five'
+console.log(isFive(13));     // => null
+console.log(isFive("apple")) // => null
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = isFive;
