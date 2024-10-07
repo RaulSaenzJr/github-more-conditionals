607c73decfc47f5fade9facd05d8d14519fe1caa
RaulSaenzJr <raul.saenz2@gmail.com>
2024-10-07 13:47:53 -0500

    created a function called isValidSubStr

--- problems/05-is-valid-substring.js
+++ problems/05-is-valid-substring.js
@@ -6,11 +6,21 @@ return "INVALID".
 */
 
 // Your code here 
+// Define a function isValidSubStr that takes in two string parameters
 
-// console.log(isValidSubStr("JOY", "joy"));                     // => 'VALID'
-// console.log(isValidSubStr("The cat jumped!", "he cat jump")); // => 'VALID'
-// console.log(isValidSubStr("Time to program", "time"));        // => 'VALID'
-// console.log(isValidSubStr("happy", "happiness"));             // => 'INVALID'
+function isValidSubStr(str1, str2) { 
+    // if the second string is part of the first string regardless of the casing of the characters, return 'VALID'
+    if (str1.toLowerCase().includes(str2.toLowerCase())) {
+        return 'VALID';
+    } else {
+        return 'INVALID';
+    }
+}
+
+console.log(isValidSubStr("JOY", "joy"));                     // => 'VALID'
+console.log(isValidSubStr("The cat jumped!", "he cat jump")); // => 'VALID'
+console.log(isValidSubStr("Time to program", "time"));        // => 'VALID'
+console.log(isValidSubStr("happy", "happiness"));             // => 'INVALID'
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = isValidSubStr;
